f445d31088f0dd2a246387fc4315710e4ff16956
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-04-29 14:07:37 +0530

    complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
 );
