7890c278c588c5a7df85ba5bf72be7f98273b5d1
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-05-02 18:58:38 +0530

    complete

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -17,7 +17,6 @@ export const useAuthStore = create((set, get) => ({
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
-      console.log(res);
 
       set({ authUser: res.data });
 
